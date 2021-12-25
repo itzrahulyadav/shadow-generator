e6650e33aac734edde2d15e7c2590b349ea75e09
itzrahulyadav <rahulkyadav9691@gmail.com>
2021-12-25 21:56:28 +0530

    updating the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [hori, setHori] = useState(10);
+  const [veri, setVeri] = useState(10);
+  const [blur, setBlur] = useState(10);
+  const [spread, setSpread] = useState(1);
+  const [color, setColor] = useState('grey');
+  const [btn, setBtn] = useState(false);
+
+  useEffect(() => {
+    alert("This little thing was created by Rahul Yadav while learning React js ❤")
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="controls">
+        <label>Horizontal length</label>
+        <input type="range" min="0" max="100" value={hori} onChange={(e) => (setHori(e.target.value))} />
+        <label>vertical length</label>
+        <input type="range" min="0" max="100" value={veri} onChange={(e) => (setVeri(e.target.value))} />
+        <label>blur</label>
+        <input type="range" min="0" max="100" value={blur} onChange={(e) => (setBlur(e.target.value))} />
+        <label>spread</label>
+        <input type="range" min="0" max="10" value={spread} onChange={(e) => (setSpread(e.target.value))} />
+        <label>color</label>
+        <input type="color" value={color} onChange={(e) => (setColor(e.target.value))} />
+        <div className="check">
+          <label>inset</label>
+          <input type="checkbox" checked={btn} onChange={() => (setBtn(!btn))} />
+
+        </div>
+      </div>
+      <div className="output">
+        <div className="box" style={{ boxShadow: `${btn ? "inset" : ""} ${hori}px ${veri}px ${blur}px ${spread}px ${color}` }}>
+          <div className="internal">
+            <p>boxShadow : {hori}px {veri}px {blur}px {spread}px {color}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
+
+
 }
 
 export default App;
